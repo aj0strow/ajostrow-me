7fe3d4cc331051fe41dac1bc9e2f49f6896a7f65
AJ Ostrow <ajsonics8@yahoo.com>
2012-08-07 01:50:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjostrowMe::Application.config.secret_token = '23637dabe1cb6a1e94d2c867745b68c2a555c46ba038461647f2e6bbb0c6af89dc26bcdf7f316101b205340ab83aa8f6e4159ddc32d5001361ced7ad8ce61f72'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+AjostrowMe::Application.routes.draw do
+ 
+  root to: 'Pages#home'
+ 
+end
