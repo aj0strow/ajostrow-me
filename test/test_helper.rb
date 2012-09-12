73a30fb6f44cb869d005163ee843a1c878033324
AJ Ostrow <ajsonics8@yahoo.com>
2012-09-12 04:53:32 -0400

    restyled and refactored

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   
   def login_with_facebook
-    get '/auth/facebook'
+    get '/auth/facebook', controller: :sessions
   end
   
   OmniAuth.config.test_mode = true
