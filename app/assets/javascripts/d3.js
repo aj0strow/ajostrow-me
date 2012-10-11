32a1ab58e366b86917137029d221072307665d19
aj0strow <alexander.ostrow@gmail.com>
2012-10-11 07:45:25 -0400

    now precompiling d3 scripts

---
+++ app/assets/javascripts/d3.js
@@ -0,0 +1,4 @@
+// This is a manifest file for the d3 folder
+//
+//= require ./d3/d3.v2.min
+//= require ./d3/d3.layout.cloud
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ AjostrowMe::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( d3.js )
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
