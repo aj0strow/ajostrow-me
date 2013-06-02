1a848aa00cf90392c8fb80b8e64d2aa6af411847
aj0strow <alexander.ostrow@gmail.com>
2013-06-02 19:20:24 -0400

    modified experience and about

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,4 +6,21 @@ module PagesHelper
     end
   end
   
+  def experience_section(options = {}, &block)
+    @flag ||= false
+    @flag = !@flag
+    
+    content = content_tag :div, class: 'content' do
+      content_tag :div, &block
+    end
+    
+    img = content_tag :div, class: 'image' do
+      image_tag("experience/#{options[:image]}") + content_tag(:span, options[:caption])
+    end
+    
+    section = content_tag :section, class: 'row experience-section' do
+      @flag ? content + img : img + content
+    end
+  end
+  
 end
